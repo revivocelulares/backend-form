c97324f6fb74843c96d4173f8e641d6cd3a4510e
Diego Ferreira <revivocelulares@gmail.com>
2023-03-10 19:58:55 -0300

    Verify Email

--- api/src/controllers/Inscripto.js
+++ api/src/controllers/Inscripto.js
@@ -6,26 +6,26 @@ const inscripto = {
     addNewInscripto: async (req, res) => {
         try {
             let { email, nombre, apellido, pais, profesion, isResident, isMember } = req.body;
-
-            const connect = await mysql.createConnection({
-                host: SIBEN_DB_HOST,
-                user: SIBEN_DB_USER,
-                password: SIBEN_DB_PASSWORD,
-                database: SIBEN_DB_NAME
-            });
-            const [rows, fields] = await connect.execute(`SELECT * FROM users WHERE email=? AND group_id=3 AND active=1`, [email]);
-            const verify_email = rows.length > 0 ? rows[0]?.email : null;
-            let isMember1 = verify_email != null ? 1 : 0;
-
-            if(isMember === isMember1) {
-                isMember = 1;
-            } else {
-                isMember = 0;
+            if(isMember === 1) {
+                const connect = await mysql.createConnection({
+                    host: SIBEN_DB_HOST,
+                    user: SIBEN_DB_USER,
+                    password: SIBEN_DB_PASSWORD,
+                    database: SIBEN_DB_NAME
+                });
+                const [rows, fields] = await connect.execute(`SELECT * FROM users WHERE email=? AND group_id=3 AND active=1`, [email]);
+                const verify_email = rows.length > 0 ? rows[0]?.email : null;
+                console.log('VERIFY_EMAIL: ' + verify_email);
+                if(verify_email === email) {
+                    isMember = 1;
+                } else {
+                    isMember = 0;
+                }               
             }
-
             const query = "CALL sp_crear_inscripto(?,?,?,?,?,?,?)";
 
             const conn = await dbconn();
+            console.log('isMember ---------------- ' + isMember);
             conn.query(query, [email, nombre, apellido, pais, profesion, isResident, isMember],
                 (error, results, fields) => {
                     if(error) {
