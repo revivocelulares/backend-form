fd80803dfd1f107d6434c40846c728ae50ae0b12
Diego Ferreira <revivocelulares@gmail.com>
2023-03-09 23:34:19 -0300

    Much More Magical Code

---
+++ api/src/routes/dolar.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+const route = require("express").Router();
+
+route.get('/', async (req, res) => {
+    const dolar = await axios.get('https://api.estadisticasbcra.com/usd_of_minorista',
+    {
+        headers: {
+            'Authorization': `Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDQ1MDg4ODIsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJyb21pbmFib2NvbkBob3RtYWlsLmNvbSJ9.G_b-fkqHKiL2gU2ncPwOfNI_BTJ46eidbueziqCrCtNMiXSvZskqM0btOqHSSKPyVuVHLcu4220H71YQS1geZg`,
+        },
+    });
+    return res.status(200).send(dolar.data[dolar.data.length - 1]);
+});
+
+module.exports = route;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,11 +4,13 @@ const congreso = require('./congreso');
 const inscripto = require('./inscripto');
 const comprobante = require('./comprobante');
 const resp_pago = require('./resp_pago');
+const dolar = require('./dolar');
 
 router.use('/process-payment', payment);
 router.use('/congreso', congreso);
 router.use('/inscripto', inscripto);
 router.use('/comprobante', comprobante);
 router.use('/resp_pago', resp_pago);
+router.use('/dolar', dolar);
 
 module.exports = router;
