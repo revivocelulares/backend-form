7089df3d2ac5abc877241ee8bd68587105b86afe
Diego Ferreira <revivocelulares@gmail.com>
2023-10-19 14:43:58 -0300

    Update dolar.js

--- api/src/routes/dolar.js
+++ api/src/routes/dolar.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const route = require("express").Router();
 
 route.get('/', async (req, res) => {
-    const dolar = await axios.get('https://cotizaciones-o14m-dev.fl0.io//api/dolaroficial');
+    const dolar = await axios.get('https://dolar.wwl.com.ar/api/dolaroficial');
     return res.status(200).json(dolar.data);
 });
 
